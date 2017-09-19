dd77f0a3fb8010e88e02f85548b35c718d734488
jigsaw <ivos.jovan@gmail.com>
2017-09-19 13:58:47 +0200

    Option to chose tags, optional class, export to file( only placeholder )  Changes to be committed: 	modified:   scrape_gui.ui 	modified:   scraper.py 	modified:   test.txt

--- scraper.py
+++ scraper.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'scrape_gui.ui'
-#
-# Created by: PyQt4 UI code generator 4.11.4
-#
-# WARNING! All changes made in this file will be lost!
 
 import sys
 import requests
@@ -29,11 +22,15 @@ except AttributeError:
 
 
 class Ui_MainWindow(object):
+
+    checkState = False
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
         MainWindow.resize(800, 600)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+
         self.lineEdit = QtGui.QLineEdit(self.centralwidget)
         self.lineEdit.setGeometry(QtCore.QRect(20, 30, 391, 29))
         self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
@@ -49,8 +46,42 @@ class Ui_MainWindow(object):
         self.frame.setFrameShadow(QtGui.QFrame.Raised)
         self.frame.setObjectName(_fromUtf8("frame"))
         self.textEdit = QtGui.QTextEdit(self.frame)
-        self.textEdit.setGeometry(QtCore.QRect(10, 10, 481, 451))
+        self.textEdit.setGeometry(QtCore.QRect(10, 50, 481, 411))
         self.textEdit.setObjectName(_fromUtf8("textEdit"))
+        self.label = QtGui.QLabel(self.frame)
+        self.label.setGeometry(QtCore.QRect(27, 9, 461, 31))
+        self.label.setStyleSheet(_fromUtf8(""))
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName(_fromUtf8("label"))
+        # TAG
+        self.lineEdit_2 = QtGui.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(540, 110, 71, 29))
+        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
+
+        self.label_2 = QtGui.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(542, 90, 68, 17))
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.label_3 = QtGui.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(655, 90, 101, 17))
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+
+        self.lineEdit_3 = QtGui.QLineEdit(self.centralwidget)
+        self.lineEdit_3.setGeometry(QtCore.QRect(650, 110, 113, 29))
+        self.lineEdit_3.setObjectName(_fromUtf8("lineEdit_3"))
+
+        self.checkBox = QtGui.QCheckBox(self.centralwidget)
+        self.checkBox.setGeometry(QtCore.QRect(540, 220, 121, 22))
+        self.checkBox.setObjectName(_fromUtf8("checkBox"))
+        self.checkBox.clicked.connect(self.check_export)
+
+        self.lineEdit_4 = QtGui.QLineEdit(self.centralwidget)
+        self.lineEdit_4.setGeometry(QtCore.QRect(540, 180, 241, 29))
+        self.lineEdit_4.setObjectName(_fromUtf8("lineEdit_4"))
+        self.lineEdit_4.setEnabled(self.checkState)
+
+        self.label_4 = QtGui.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(540, 160, 68, 17))
+        self.label_4.setObjectName(_fromUtf8("label_4"))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 23))
@@ -67,20 +98,43 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
         self.lineEdit.setPlaceholderText(_translate("MainWindow", "Enter URL to scrape from", None))
         self.pushButton.setText(_translate("MainWindow", "Scrape", None))
+        self.label.setText(_translate("MainWindow", "PREVIEW", None))
+        self.lineEdit_2.setToolTip(_translate("MainWindow", "Enter desired tag for scraping", None))
+        self.lineEdit_2.setPlaceholderText(_translate("MainWindow", "Tag", None))
+        self.label_2.setText(_translate("MainWindow", "Tag name", None))
+        self.label_3.setText(_translate("MainWindow", "Class(optional)", None))
+        self.lineEdit_3.setToolTip(_translate("MainWindow", "Leave blank if none", None))
+        self.lineEdit_3.setPlaceholderText(_translate("MainWindow", "Class", None))
+        self.checkBox.setText(_translate("MainWindow", "Export to file", None))
+        self.lineEdit_4.setToolTip(_translate("MainWindow", "Name of exported file", None))
+        self.lineEdit_4.setPlaceholderText(_translate("MainWindow", "Name of file", None))
+        self.label_4.setText(_translate("MainWindow", "Filename:", None))
 
     def scrape(self):
         url = self.lineEdit.text()
+        tag = self.lineEdit_2.text()
+        tag_class = self.lineEdit_3.text()
         r = requests.get("http://" + url)
         data = r.text
         soup = bs(data, "html.parser")
 
         #html = soup.getText().encode('utf-8')
         #f = open('test.txt', 'a')
-        for link in soup.findAll('a'):
-            linkText = unicode(link).encode('utf-8')
-            self.textEdit.append(linkText)
+        self.textEdit.clear()
+        if(self.lineEdit_3.text().isEmpty()):
+            for link in soup.findAll(tag):
+                linkText = unicode(link).encode('utf-8')
+                self.textEdit.append(linkText)
+        else:
+            for link in soup.findAll(tag, {"class": tag_class}):
+                linkText = unicode(link).encode('utf-8')
+                self.textEdit.append(linkText)
             #f.write(linkText + "\n")
         # f.close()
+
+    def check_export(self):
+        self.checkState = not self.checkState
+        self.lineEdit_4.setEnabled(self.checkState)
 
 
 if __name__ == "__main__":
